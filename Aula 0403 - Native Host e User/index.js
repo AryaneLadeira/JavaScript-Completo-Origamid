1203590032ce75588e3fe059bfdec9df53acb87b
Aryane Ladeira <69313233+AryaneLadeira@users.noreply.github.com>
2021-10-26 14:44:00 -0300

    Concluído: Aula de Native, Host e User

---
+++ Aula 0403 - Native Host e User/index.js
@@ -0,0 +1,10 @@
+// EXERCICIO 1
+
+    // OBJETOS NATIVOS:
+//      Error, Boolean, Function, NaN, Undefined
+
+//     // OBJETOS DO BROWSER:
+//      Window, NodeList, Document, Element, HTMLCollection
+
+// // EXERCICIO 2
+//     webkitVisibilityState, webkitHidden
